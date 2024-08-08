b0aa9e5f3fe004f8371e3bcdc935fe7781a932e5
Yuvraj Gupta <96349681+yuvrajthegupta@users.noreply.github.com>
2024-08-08 14:58:42 +0530

    Update user-schema.js

--- DB/modules/user-schema.js
+++ DB/modules/user-schema.js
@@ -1,13 +1,25 @@
 import  { SchemaTypes } from "mongoose";
 import mongoose from "../connections.js";
 const schema = mongoose.Schema;
+
+const slotSchema = new Schema({
+    time : {
+        type: String,
+    },
+    isBooked : {
+        type: Boolean,
+        default: false
+    }
+})
+
 const userschema = new schema({
   'email': { type: SchemaTypes.String, required: true, unique: true },
   'password': { type: SchemaTypes.String, required: true, minLength : 8,maxLength : 100 },
   'name': { type: SchemaTypes.String, required: true },
   'phone': { type: SchemaTypes.String },
   'info': { type: SchemaTypes.String },
-  'flag':{type:SchemaTypes.Boolean}
+  'flag':{type:SchemaTypes.Boolean},
+  'dates' : [dateSchedule]
 });
 const usermodel = mongoose.model("user", userschema);
 export default usermodel;
